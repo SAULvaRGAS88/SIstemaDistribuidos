a6cf35ecce6bb2e8282a26a6e56673d412df3791
Saul <saulvargascosta@hotmail.com>
2023-10-25 22:38:16 -0300

    int

--- ChoraNenem/client.py
+++ ChoraNenem/client.py
@@ -2,12 +2,12 @@ import Pyro4 as pyro
 
 if __name__ == '__main__':
     uri = "PYRONAME:Cantina@localhost:9090"
-    a = input("""
-        Menu:
-        1-Hamburguer.
-        2-Refrigerante.
-        3-Batata frita.
-        """ ).strip()
+    a = int(input("""
+    Menu:
+    1-Hamburguer.
+    2-Refrigerante.
+    3-Batata frita.
+    """ ).strip())
     
     math = pyro.Proxy(uri) 
     result = math.cardapio(a) 
